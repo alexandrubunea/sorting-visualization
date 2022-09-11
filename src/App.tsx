c690fd3edd2e5b9b09db4e25736cb58d398d459e
Bunea Alexandru <alexandrubunea03@gmail.com>
2022-09-11 08:22:55 +0300

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 // Components
 import Container from "./components/Container";
@@ -21,43 +21,84 @@ import './App.css';
 		top_gap = the size of the top margin
 */
 // Const Values
-const tool_bar_height = Math.floor(window.innerHeight * 5/100);
+const tool_bar_height = Math.floor(window.innerHeight * 5/100) + 30;
 const bar_max_value = window.innerHeight - tool_bar_height - 100;
 const bar_width = 5;
 const gap_between_bars = 2;
 const number_of_bars = Math.floor((window.innerWidth - Math.floor((window.innerWidth / 10)) - 25) / (gap_between_bars + bar_width));
 const top_gap = window.innerHeight - bar_max_value - tool_bar_height;
 
+const start_sort = (arr: number[], sort_type: number) => {
+	let sorted_arr: number[] = [];
+	sorted_arr = [...arr];
+
+	switch(sort_type) {
+		case 1: {
+			
+			break;
+		}
+		case 2: {
+
+			break;
+		}
+		case 3: {
+
+			break;
+		}
+		case 4: {
+
+			break;
+		}
+	}
+
+	return sorted_arr;
+}
+
+const fill_random_values = () => {
+	let arr:number[] = [];
+
+	for(let i = 0; i < number_of_bars; ++i) {
+		let random_value = Math.floor(Math.random() * (bar_max_value - 25 + 1) + 25);
+		arr.push(random_value);
+	}
+
+	arr[Math.floor((Math.random() * (arr.length - 1)) + 1)] = bar_max_value;
+
+	return arr;
+}
 
 const App = () => {
 	const [random_values, set_random_values] = useState<number[]>([]);
+	const algorithm_selected = useRef(document.createElement("select"))
 
 	const ToolbarStyle = {
 		height: tool_bar_height + "px",
 	};
 
-	const reset_random_values = () => {
-		let arr:number[] = [];
-
-		for(let i = 0; i < number_of_bars; ++i) {
-			let random_value = Math.floor(Math.random() * (bar_max_value - 25 + 1) + 25);
-			arr.push(random_value);
-		}
-
-		arr[Math.floor((Math.random() * (arr.length - 1)) + 1)] = bar_max_value;
-		set_random_values(arr);
-	};
-
 	useEffect(() => {
-		reset_random_values();
-	}, []);
+		set_random_values(fill_random_values());
+	}, [set_random_values]);
+
+	
 
 	
 	return (
 		<div className='App'>
 			<Container array={random_values} top_margin={top_gap} container_height={bar_max_value} />
 			<div className="toolbar" style={ToolbarStyle}>
-				<button onClick={() => {reset_random_values()}}>Reset</button>
+				<div className="content">
+					<label htmlFor="algorithm">Sorting Algorithm:</label>
+					<select id="algorithm" ref={algorithm_selected!}>
+						<option value="1">Selection Sort</option>
+						<option value="2">Bubble Sort</option>
+						<option value="3">Merge Sort</option>
+						<option value="4">Quick Sort</option>
+					</select>
+					<button onClick={() => {
+						set_random_values(start_sort(random_values, Number(algorithm_selected.current.value)));
+					}}>Sort</button>
+					<button onClick={() => {set_random_values(fill_random_values())}}>Reset</button>
+				</div>
 			</div>
 		</div>
   	)
